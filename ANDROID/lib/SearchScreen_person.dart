ad19eea317c0ce9d813c813c2f0d51407a009705
jorpac <36743869+jorpac@users.noreply.github.com>
2019-11-30 22:09:18 +0000

    Add files via upload

--- ANDROID/lib/SearchScreen_person.dart
+++ ANDROID/lib/SearchScreen_person.dart
@@ -15,23 +15,60 @@ class SearchScreen_person extends StatefulWidget {
   _SearchScreen_person createState() => new _SearchScreen_person();
 
 }
-
+List userList =[];
 
 
 class _SearchScreen_person extends State<SearchScreen_person> {
 
   static var geolocator = Geolocator();
   static var locationOptions = LocationOptions(accuracy: LocationAccuracy.best, distanceFilter: 1);
-  static var user_pos = [];
+  static var user_pos = [feup_loc.elementAt(0), feup_loc.elementAt(1)];
 
   StreamSubscription<Position> positionStream = geolocator.getPositionStream(
       locationOptions).listen(
-          (Position position) => {user_pos.add(position.latitude),
-        user_pos.add(position.longitude)});
+          (Position position) => {user_pos.insert(0, position.latitude),
+        user_pos.insert(1, position.longitude)});
 
   static Future<Position> position = geolocator.getCurrentPosition();
   Completer<GoogleMapController> _controller = Completer();
-  CameraPosition user = CameraPosition(target: LatLng(feup_loc.elementAt(0), feup_loc.elementAt(1)), zoom: 17);
+  CameraPosition user = CameraPosition(target: LatLng(user_pos.elementAt(0), user_pos.elementAt(1)), zoom: 17);
+  CameraPosition final_user;
+
+  final controller = TextEditingController();
+
+  /// Function that displays the possible speakers when as users writes each letter
+  onChangeSpeakerSearch() {
+    controller.text;
+    userList = [];
+    for(final userr in users)
+    {
+      String username = userr[0];
+        if (username.toUpperCase().contains(controller.text.toUpperCase())) {
+          userList.add(userr);
+
+      }
+    }
+
+    for(final i in userList)  // DEBUG.
+        {
+      debugPrint('DEBUG' + i[0]);
+      CameraPosition _final_user = CameraPosition(target: LatLng(i[1], i[2]), zoom: 17);
+      final_user = _final_user;
+    }
+  }   /// End of function
+  void initState() {
+    controller.addListener(() {
+      //print(controller.text); // DEBUG
+      onChangeSpeakerSearch();
+    });
+    super.initState();
+  }
+
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return new Scaffold(
         backgroundColor: colorPallete[900],
@@ -40,9 +77,17 @@ class _SearchScreen_person extends State<SearchScreen_person> {
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              new Image.asset('assets/images/mainLogo.png', fit: BoxFit.scaleDown)
 
-              ,
+              SizedBox(
+        height: MediaQuery.of(context).size.height*0.15,
+      child:
+              new TextField(
+            autofocus: true,
+              controller: controller,
+              style: subTitleStyle,
+            )
+              )
+        ,
 
               SizedBox(
                   height:MediaQuery.of(context).size.height*0.5,
--- ANDROID/lib/users.dart
+++ ANDROID/lib/users.dart
@@ -1,4 +1,6 @@
-var users = [ ["Joao Antonio", 41.0, -8.15], ["Antonio Manuel", 40.6,-8.19], ["Manuel Joao", 41.2, -8.3]
+var users = [ ["JOAO ANTONIO", 41.0, -8.15],
+  ["ANTONIO MANUEL", 40.6,-8.19],
+  ["Manuel Joao", 41.2, -8.3]
 ];
 
 var feup_loc =[41.1780, -8.5960];
